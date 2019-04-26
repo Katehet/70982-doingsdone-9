6c2bb6d8e340e619f84bb24dc3bb506cfde1729a
Katehet <sv_sleep@mail.ru>
2019-04-26 15:28:30 +0300

    Добавляет тренировочные запросы

--- queries.sql
+++ queries.sql
@@ -16,7 +16,7 @@ SET reg_date = NOW(),
 
 /* Заполняет таблицу projects */
 INSERT INTO projects (project_name, user_id)
-VALUES ("Входящие", "1"), ("Учеба", "1"), ("Работа", "1"), ("Домашние дела", "1"), ("Авто", "1");
+VALUES ("Входящие", "1"), ("Учеба", "1"), ("Работа", "1"), ("Домашние дела", "2"), ("Авто", "1");
 
 /* Заполняет таблицу tasks */
 INSERT INTO tasks
@@ -40,7 +40,7 @@ SET task_name = "Сделать задание первого раздела",
     task_timeout = "21.12.2018",
     user_id = "1",
     project_id = "2",
-    task_status = "0",
+    task_status = "1",
     add_date = NOW();
 
 INSERT INTO tasks
@@ -54,7 +54,7 @@ SET task_name = "Встреча с другом",
 INSERT INTO tasks
 SET task_name = "Купить корм для кота",
     task_timeout = "Нет",
-    user_id = "1",
+    user_id = "2",
     project_id = "4",
     task_status = "0",
     add_date = NOW();
@@ -66,3 +66,28 @@ SET task_name = "Заказать пиццу",
     project_id = "4",
     task_status = "0",
     add_date = NOW();
+
+/* Получить список из всех проектов для одного пользователя */
+SELECT user_name, p.project_name, p.project_id FROM projects p
+JOIN users u
+ON u.user_id = p.user_id
+WHERE u.user_id = 1;
+
+/* Посчитать количество задач в каждом проекте  */
+SELECT t.task_id, t.task_name, p.project_name FROM tasks t
+JOIN projects p
+ON t.project_id = p.project_id;
+
+/* Получить список из всех задач для одного проекта */
+SELECT p.project_name, t.task_name FROM tasks t
+JOIN projects p
+ON t.project_id = p.project_id
+WHERE p.project_id = 3;
+
+/* пометить задачу как выполненную */
+UPDATE tasks SET task_status = "1"
+WHERE task_name = "Заказать пиццу";
+
+/* обновить название задачи по её идентификатору */
+UPDATE tasks SET task_name = "Заказать такси"
+WHERE task_id = "6";
