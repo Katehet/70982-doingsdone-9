cd19cfb62fc9704277c1aa24c6cc2bacbac2d1c7
Katehet <sv_sleep@mail.ru>
2019-05-06 19:17:44 +0300

    Выводит список задач для каждого проекта

--- index.php
+++ index.php
@@ -9,8 +9,19 @@ require_once("helpers.php");
 $query_proj = "SELECT p.project_id, p.project_name FROM projects p JOIN users u ON u.user_id = p.user_id WHERE u.user_id = $user_id";
 $projects = get_query_result($connect, $query_proj);
 
-/* Получить список из всех задач для одного пользователя */
-$query_tasks = "SELECT t.task_name, t.task_timeout, p.project_name, t.task_status FROM tasks t JOIN projects p ON t.project_id = p.project_id WHERE p.user_id = $user_id";
+$project_id;
+
+if(isset($_GET["project_id"])) {
+    $project_id = $_GET["project_id"];
+
+    /* Получить список задач для выбранного проекта */
+    $query_tasks = "SELECT t.task_name, t.task_timeout, p.project_name, t.task_status FROM tasks t JOIN projects p ON t.project_id = p.project_id WHERE p.project_id = $project_id";
+
+}
+else {
+    /* Получить список из всех задач для одного пользователя */
+    $query_tasks = "SELECT t.task_name, t.task_timeout, p.project_name, t.task_status FROM tasks t JOIN projects p ON t.project_id = p.project_id WHERE p.user_id = $user_id";
+}
 $tasks = get_query_result($connect, $query_tasks);
 
 $page_content = include_template("index.php", ["projects" => $projects, "tasks" => $tasks, "show_complete_tasks" => $show_complete_tasks]);
