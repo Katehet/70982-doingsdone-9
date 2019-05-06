b376b859bc4b337a8688391e46196e9389e42152
Katehet <sv_sleep@mail.ru>
2019-05-06 19:19:27 +0300

    Форматирует для удобства код запросов

--- index.php
+++ index.php
@@ -6,7 +6,11 @@ require_once("functions.php");
 require_once("helpers.php");
 
 /* Получить список из всех проектов для одного пользователя */
-$query_proj = "SELECT p.project_id, p.project_name FROM projects p JOIN users u ON u.user_id = p.user_id WHERE u.user_id = $user_id";
+$query_proj = "SELECT p.project_id, p.project_name FROM projects p "
+            . "JOIN users u "
+            . "ON u.user_id = p.user_id "
+            . "WHERE u.user_id = $user_id";
+
 $projects = get_query_result($connect, $query_proj);
 
 $project_id;
@@ -15,13 +19,19 @@ if(isset($_GET["project_id"])) {
     $project_id = $_GET["project_id"];
 
     /* Получить список задач для выбранного проекта */
-    $query_tasks = "SELECT t.task_name, t.task_timeout, p.project_name, t.task_status FROM tasks t JOIN projects p ON t.project_id = p.project_id WHERE p.project_id = $project_id";
-
+    $query_tasks = "SELECT t.task_name, t.task_timeout, p.project_name, t.task_status FROM tasks t "
+                . "JOIN projects p "
+                . "ON t.project_id = p.project_id "
+                . "WHERE p.project_id = $project_id";
 }
 else {
     /* Получить список из всех задач для одного пользователя */
-    $query_tasks = "SELECT t.task_name, t.task_timeout, p.project_name, t.task_status FROM tasks t JOIN projects p ON t.project_id = p.project_id WHERE p.user_id = $user_id";
+    $query_tasks = "SELECT t.task_name, t.task_timeout, p.project_name, t.task_status FROM tasks t "
+                . "JOIN projects p "
+                . "ON t.project_id = p.project_id "
+                . "WHERE p.user_id = $user_id";
 }
+
 $tasks = get_query_result($connect, $query_tasks);
 
 $page_content = include_template("index.php", ["projects" => $projects, "tasks" => $tasks, "show_complete_tasks" => $show_complete_tasks]);
