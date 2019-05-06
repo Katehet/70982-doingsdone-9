0df5dd1beaa13b4693a6b207d63a944391b089bc
Katehet <sv_sleep@mail.ru>
2019-05-06 19:54:40 +0300

    Выводит количество задач в проекте независимо от выбранного проекта

--- index.php
+++ index.php
@@ -19,23 +19,24 @@ if(isset($_GET["project_id"])) {
     $project_id = $_GET["project_id"];
 
     /* Получить список задач для выбранного проекта */
-    $query_tasks = "SELECT t.task_name, t.task_timeout, p.project_name, t.task_status FROM tasks t "
+    $project_tasks = "SELECT t.task_name, t.task_timeout, p.project_name, t.task_status FROM tasks t "
                 . "JOIN projects p "
                 . "ON t.project_id = p.project_id "
                 . "WHERE p.project_id = $project_id";
 }
-else {
-    /* Получить список из всех задач для одного пользователя */
-    $query_tasks = "SELECT t.task_name, t.task_timeout, p.project_name, t.task_status FROM tasks t "
-                . "JOIN projects p "
-                . "ON t.project_id = p.project_id "
-                . "WHERE p.user_id = $user_id";
-}
 
-$tasks = get_query_result($connect, $query_tasks);
+$tasks_in_project = get_query_result($connect, $project_tasks);
 
-$page_content = include_template("index.php", ["projects" => $projects, "tasks" => $tasks, "show_complete_tasks" => $show_complete_tasks]);
-$layout_content = include_template("layout.php", ["main_content" => $page_content, "title" => "Дела в порядке", "user_name" => $user_name, "projects" => $projects, "tasks" => $tasks]);
+/* Получить список из всех задач для одного пользователя */
+$user_tasks = "SELECT t.task_name, t.task_timeout, p.project_name, t.task_status FROM tasks t "
+            . "JOIN projects p "
+            . "ON t.project_id = p.project_id "
+            . "WHERE p.user_id = $user_id";
+
+$tasks_for_user = get_query_result($connect, $user_tasks);
+
+$page_content = include_template("index.php", ["projects" => $projects, "tasks" => $tasks_in_project, "show_complete_tasks" => $show_complete_tasks]);
+$layout_content = include_template("layout.php", ["main_content" => $page_content, "title" => "Дела в порядке", "user_name" => $user_name, "projects" => $projects, "tasks" => $tasks_for_user]);
 print($layout_content);
 
 ?>
