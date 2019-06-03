8ea0fcef70723bd27f0a4f39d4fcadc18760438f
Katehet <sv_sleep@mail.ru>
2019-06-03 21:53:18 +0300

    Меняет концепцию формирования рассылки - Нексолько задач в одном письме

--- notify.php
+++ notify.php
@@ -13,51 +13,45 @@ $transport->setPassword("htmlacademy");
 $mailer = new Swift_Mailer($transport);
 
 /* Формирование сообщения */
-$sql = "SELECT task_name, task_timeout, u.email, u.user_name FROM tasks t
-        JOIN users u ON t.user_id = u.user_id 
-        WHERE t.task_status = '0' AND t.task_timeout = CURDATE() 
-        ORDER BY t.add_date";
-
-$res = mysqli_query($connect, $sql);
+$res = mysqli_query($connect, "SELECT email, user_id, user_name FROM users");
 
 if($res && mysqli_num_rows($res)) {
-    $task_list = mysqli_fetch_all($res, MYSQLI_ASSOC);
+    $users = mysqli_fetch_all($res, MYSQLI_ASSOC);
 
-    $res = mysqli_query($connect, "SELECT email, user_name FROM users");
+    /* Получение списка пользователей с email */
+    foreach ($users as $user) {
+        $recipients= [
+            $user["email"] => $user["user_name"]
+        ];
 
-    if($res && mysqli_num_rows($res)) {
-        $users = mysqli_fetch_all($res, MYSQLI_ASSOC);
-        $recipients = [];
+        $sql = "SELECT task_name, task_timeout FROM tasks 
+                WHERE user_id = " . $user['user_id'] . "
+                AND task_status = '0' AND task_timeout = CURDATE()";
+        $res = mysqli_query($connect, $sql);
+        if($res && mysqli_num_rows($res)) {
+            $task_list = mysqli_fetch_all($res, MYSQLI_ASSOC);
 
-        /* Получение списка пользователей с email */
-        foreach ($users as $user) {
-            $recipients[$user["email"]] = $user["user_name"];
-        }
-        
-        foreach ($task_list as $key => $value) {
-            $name = $value['user_name'];
-            $task = $value['task_name'];
-            $time = $value['task_timeout'];
-                        
+            $msg = "<br><br>Уважаемый " . $user['user_name'] . "! У Вас запланированы следующие задачи: ";
+            foreach ($task_list as $key => $value) {
+                $msg .= "<br>- " . $value['task_name']. " на " . $value['task_timeout'];
+            }
             $message = new Swift_Message();
             $message->setSubject("Уведомление от сервиса «Дела в порядке»");
             $message->setFrom(["keks@phpdemo.ru"=> "Дела в порядке"]);
             $message->setBcc($recipients);
             
-            $msg_content = "Уважаемый $name! У вас запланирована задача '$task' на $time";
-            $message->setBody($msg_content, "text/plain");
+            $message->setBody($msg, "text/html");
             
             /* Отправка сообщения */
             $result = $mailer->send($message);
+            
+            if($result) {
+                print("Рассылка успешно отправлена!");
+            }
+            else {
+                print("Не удалось отправить рассылку.");
+            }
         }
-
-        if($result) {
-            print("Рассылка успешно отправлена!");
-        }
-        else {
-            print("Не удалось отправить рассылку.");
-        }
-
     }
 }
 
